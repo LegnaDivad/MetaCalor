c79131981082f47c2f1cd524ad4b79876cd29a80
ArmandoRODXG <143852331+ArmandoRODXG@users.noreply.github.com>
2023-11-23 19:20:05 -0600

    Lógica de días.
    Commit de lógica de días.
    
    - Se añadió la lógica de días para mostrar los alimentos correspondientes.
    - Se añadió un botón para poder adelantar los días para hacer pruebas.
    - Se añadió una consulta para obtener los alimentos registrados de la semana.

--- Informe.py
+++ Informe.py
@@ -3,18 +3,41 @@ import flet as ft
 from flet_core.control import Control, OptionalNumber
 from flet_core.ref import Ref
 from flet_core.types import AnimationValue, ClipBehavior, OffsetValue, ResponsiveNumber, RotateValue, ScaleValue
+import datetime
+from Database import UserDatabase
 
 class Informe(ft.UserControl):
     def __init__(self, route):
         super().__init__()
         self.route = route
         
+        
+        self.kcalTotales = ft.Text(value='hola',color='black')
+        
+        
         self.index = ft.Container(
-            expand=True
+            expand=True,
+            content=self.kcalTotales
         )
+        
+    def generarInforme(self,e):
+        pass
+    
+    def obtenerInfo(self):
+        fecha_actual = datetime.datetime.now()
+        # fecha = datetime.datetime(fecha_actual.year, fecha_actual.month, 19)
+        mydb = UserDatabase(self.route)
+        mydb.connect()
+        resultado = mydb.obtenerRegistrosSemana(self.route.getId(),fecha_actual)
+        mydb.close()
+        suma = 0
+        for dato in resultado:
+            suma += dato[0]
+        print(suma)
         
     def build(self):
         return self.index
     
     def inicializar(self):
+        self.obtenerInfo()
         print('Inicializando Informe')
