ffcfcf2716504d840f42f3c17134a3e9cb8e3b86
JGarcia8797 <128440116+JGarcia8797@users.noreply.github.com>
2023-11-27 02:48:49 -0600

    Update AlertDialog.py

--- AlertDialog.py
+++ AlertDialog.py
@@ -54,3 +54,75 @@ class RegisterDialog(ft.AlertDialog):
     def canceled(self, e):
         self.open = False
         self.update()
+
+class EliminacionDialog(ft.AlertDialog):
+    def __init__(self, function, title=""):
+        super().__init__()
+        self.function = function
+        self.modal=True
+        self.title=ft.Text(title)
+        self.actions=[
+            ft.TextButton(content=ft.Text("Regresar", color="blue"), on_click=self.canceled),
+            ft.TextButton(content=ft.Text("Eliminar", color="red"), on_click=self.confirmed),
+        ]
+        self.actions_alignment=ft.MainAxisAlignment.END
+        self.shape=ft.RoundedRectangleBorder(radius=10)
+
+    def build(self):
+        return self
+    
+    def confirmed(self, e):
+        self.open = False
+        self.update()
+        self.function(self.data)
+    
+    def canceled(self, e):
+        self.open = False
+        self.update()
+
+class ActualizacionDialog(ft.AlertDialog):
+    def __init__(self, function, contenido, title=""):
+        super().__init__()
+        self.function = function
+
+        self.modal=True
+        self.title=ft.Text(title)
+        self.content=contenido
+        self.actions=[
+            ft.TextButton(content=ft.Text("Cancelar", color="red"), on_click=self.canceled),
+            ft.TextButton(content=ft.Text("Modificar", color="Green"), on_click=self.confirmed),
+        ]
+        self.actions_alignment=ft.MainAxisAlignment.END
+        self.shape=ft.RoundedRectangleBorder(radius=10)
+
+    def build(self):
+        return self
+    
+    def confirmed(self, e):
+        self.open = False
+        self.update()
+        self.function(self.data)
+    
+    def canceled(self, e):
+        self.open = False
+        self.update()
+
+class PlatilloDialog(ft.AlertDialog):
+    def __init__(self, contenido, title=""):
+        super().__init__()
+        
+        self.modal=True
+        self.title=ft.Text(title)
+        self.content=contenido
+        self.actions=[
+            ft.TextButton(content=ft.Text("Regresar", color="red"), on_click=self.canceled)
+        ]
+        self.actions_alignment=ft.MainAxisAlignment.END
+        self.shape=ft.RoundedRectangleBorder(radius=10)
+
+    def build(self):
+        return self
+    
+    def canceled(self, e):
+        self.open = False
+        self.update()
