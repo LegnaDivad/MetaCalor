9f451d9ff1c9c535f3e8d6cea45fe501462ce9f0
JGarcia8797 <128440116+JGarcia8797@users.noreply.github.com>
2023-11-27 02:52:35 -0600

    Update Registro_Ejercicio.py

--- Registro_Ejercicio.py
+++ Registro_Ejercicio.py
@@ -2,6 +2,7 @@ import flet as ft
 from Cartas import CartaRegistroEjercicios
 from Database import generalDatabaseAccess
 from Notification import Notification
+from datetime import datetime
 
 class RegistroEjercicios(ft.UserControl):
     def __init__(self, route):
@@ -10,6 +11,8 @@ class RegistroEjercicios(ft.UserControl):
         
         self.GRIS = '#252422'
         self.lvEjercicios = ft.ListView(expand=True,padding=20,auto_scroll=True)
+        self.tmba = 0
+        self.tmbaText =ft.Text(value="{}".format(self.tmba),weight='bold',size=25,bgcolor=self.GRIS,color=ft.colors.DEEP_ORANGE_500)
 
         self.tmbCard = ft.Card(
             key='tmba',
@@ -25,7 +28,8 @@ class RegistroEjercicios(ft.UserControl):
                                     content=ft.Row(
                                         controls=[
                                             ft.Icon(name=ft.icons.LOCAL_FIRE_DEPARTMENT_SHARP,color=ft.colors.DEEP_ORANGE_500,size=25),
-                                            ft.Text(value='Tasa Metabolica Basal Actual',weight='bold',size=25,bgcolor=self.GRIS)
+                                            ft.Text(value='Tasa Metabolica Basal Actual:',weight='bold',size=25,bgcolor=self.GRIS),
+                                            self.tmbaText
                                         ]
                                     )
                                 )
@@ -90,6 +94,18 @@ class RegistroEjercicios(ft.UserControl):
     def buscadorEjercicios(self,e):
         self.route.buscador.establecer_Horario('cena')
         self.page.go('/buscador_ejercicios')
+
+    def updateTMBA(self):
+        mydb = generalDatabaseAccess(self.route)
+        mydb.connect()
+        self.tmba = mydb.recuperarRegistro("TMB",
+                                              "Usuario",
+                                              "ID_Usuario  = {}".format(self.route.getId()),
+                                              1)[0][0]
+        mydb.close()
+        print("TMB:",self.tmba)
+        self.tmbaText.value = self.tmba
+        self.tmbCard.update()
         
     def agregarEjercicios(self):
         self.lvEjercicios.clean()
@@ -98,15 +114,22 @@ class RegistroEjercicios(ft.UserControl):
         mydb = generalDatabaseAccess(self.route)
         mydb.connect()
         
-        actFisicaUsuario = mydb.recuperarRegistro("A.Tipo_Act, R.Duración_Act, ID_RegDeAct",
+        pesoUsuario = mydb.recuperarRegistro("Peso",
+                                              "Usuario",
+                                              "ID_Usuario  = {}".format(idUsuario) #
+                                              )
+        
+        actFisicaUsuario = mydb.recuperarRegistro("A.Tipo_Act, R.Duración_Act, ID_RegDeAct, A.MET",
                                               "Registro_Actividad_Física AS R, Actividad_Física AS A",
-                                              "R.ID_Usuario  = {} AND R.ID_AF = A.ID_AF".format(idUsuario) #AND R.Fecha_Registro = CURDATE()
+                                              "R.ID_Usuario  = {} AND R.ID_AF = A.ID_AF AND R.Fecha_Registro = '{}'".format(idUsuario,self.route.getFecha()) #
                                               )
         mydb.close()
 
         if actFisicaUsuario is not None:
             for data in actFisicaUsuario:
-                item = CartaRegistroEjercicios(self.route,data)
+                duracionHoras = data[1].total_seconds() / 3600.0
+                cal = pesoUsuario[0][0] * duracionHoras * data[3]
+                item = CartaRegistroEjercicios(self.route,data,cal)
                 self.lvEjercicios.controls.append(item)
         else:
             Notification(self.page,'Busqueda sin Resultados','red').mostrar_msg()
@@ -118,5 +141,6 @@ class RegistroEjercicios(ft.UserControl):
     
     def inicializar(self):
         self.route.page.bgcolor = '#98FB98'
+        self.updateTMBA()
         self.agregarEjercicios()
         print('Inicializando Registro Ejercicios')
